cc7c02ca88c51fa01e5317eed923fa0bd4571cf1
Seth Fischer <seth@fischer.nz>
2022-06-08 21:53:42 +1200

    build(rtd): remove hashes from requirements

--- lint-rtd-requirements.sh
+++ lint-rtd-requirements.sh
@@ -2,4 +2,4 @@
 
 set -o errexit -o noclobber -o nounset
 
-diff --to-file docs/requirements.txt <(poetry export --dev -f requirements.txt)
+diff --to-file docs/requirements.txt <(poetry export --without-hashes --dev -f requirements.txt)
