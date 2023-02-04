765458c0ffe6d14f1e1ba134fc09fc277a9540c7
Seth Fischer <seth@fischer.nz>
2023-02-04 17:38:08 +1300

    ci: check typing

--- lint-python.sh
+++ lint-python.sh
@@ -5,5 +5,6 @@ set -o errexit -o noclobber -o nounset
 directory="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 git ls-files -z -- "${directory}/**.py" | xargs --no-run-if-empty --null flake8
+git ls-files -z -- "${directory}/**.py" | xargs --no-run-if-empty --null mypy --strict
 git ls-files -z -- "${directory}/**.py" | xargs --no-run-if-empty --null black --check
 git ls-files -z -- "${directory}/**.py" | xargs --no-run-if-empty --null isort --profile black --check-only
